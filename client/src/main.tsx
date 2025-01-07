820eafa33a2df39d2350c7e6e04e5b765eda429d
HankVanRose <ScottSkcharynd@yandex.ru>
2025-01-07 12:41:32 +0500

    Bootstrap

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from '@/components/ui/provider';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </StrictMode>
 );
