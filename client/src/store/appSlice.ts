20e4aa0d9ae8a94e0e4f7f88758ed6af71a35a31
Zemfira <vildanovazemfira@gmail.com>
2025-01-08 12:05:59 +0100

    user update

--- client/src/store/appSlice.ts
+++ client/src/store/appSlice.ts
@@ -1,11 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { ExercisesType, PlansType, UserType } from '../types';
 import {
+  fetchUpdateProfile,
   fetchUserCheck,
   fetchUserLogout,
   fetchUserSignin,
   fetchUserSignup,
 } from './thunkActions';
+import { FaHouseFloodWaterCircleArrowRight } from 'react-icons/fa6';
 
 type InitialState = {
   user: UserType | null;
@@ -39,13 +41,12 @@ const appSlice = createSlice({
           state.user = null;
           state.loading = false;
         } else {
- 
           state.user = action.payload as UserType;
           state.loading = false;
           state.error = null;
- 
         }
       })
+
       .addCase(fetchUserSignin.pending, (state) => {
         state.loading = true;
         state.error = null;
@@ -56,18 +57,44 @@ const appSlice = createSlice({
           state.user = null;
           state.loading = false;
         } else {
- 
           state.user = action.payload as UserType;
           state.loading = false;
           state.error = null;
- 
         }
       })
+
       .addCase(fetchUserLogout.fulfilled, (state) => {
         state.user = initialState.user;
       })
+
       .addCase(fetchUserCheck.fulfilled, (state, action) => {
         state.user = action.payload;
+        state.loading = false;
+      })
+
+      .addCase(fetchUpdateProfile.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchUpdateProfile.fulfilled, (state, action) => {
+        if ('error' in action.payload && action.payload.error) {
+          state.error = action.payload.error;
+          state.loading = false;
+        } else {
+          if (state.user) {
+            const updatedUser = {
+              ...state.user,
+              ...action.payload,
+            } as UserType;
+            updatedUser.id = state.user.id;
+            state.user = updatedUser;
+          }
+          state.loading = false;
+          state.error = null;
+        }
+      })
+      .addCase(fetchUpdateProfile.rejected, (state) => {
+        state.error = 'failed to update profile';
         state.loading = false;
       });
   },
