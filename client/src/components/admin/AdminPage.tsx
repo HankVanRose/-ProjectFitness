a7743c485e8468e670703b70c741c6ee352c9730
renthefirst <rentraxa@gmail.com>
2025-01-10 13:52:24 +0300

    admin page user table

--- client/src/components/admin/AdminPage.tsx
+++ client/src/components/admin/AdminPage.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import PlanForm from './PlanForm';
 import ExerciseForm from './ExerciseForm';
 import { Tabs } from '@chakra-ui/react';
-import { LuDumbbell, LuClipboardList } from 'react-icons/lu';
+import { LuDumbbell, LuClipboardList, LuUsers } from 'react-icons/lu';
+import UserTable from './UserTable';
 
 export default function AdminPage() {
   return (
-    <Tabs.Root defaultValue="plans" >
+    <Tabs.Root defaultValue="plans">
       <Tabs.List p={4}>
         <Tabs.Trigger value="plans" p={2}>
           <LuClipboardList />
@@ -16,6 +17,10 @@ export default function AdminPage() {
           <LuDumbbell />
           Упражнения
         </Tabs.Trigger>
+        <Tabs.Trigger value="users" p={2}>
+          <LuUsers />
+          Пользователи
+        </Tabs.Trigger>
       </Tabs.List>
 
       <Tabs.Content value="plans">
@@ -24,6 +29,10 @@ export default function AdminPage() {
 
       <Tabs.Content value="exercises">
         <ExerciseForm />
+      </Tabs.Content>
+
+      <Tabs.Content value="users">
+        <UserTable />
       </Tabs.Content>
     </Tabs.Root>
   );
