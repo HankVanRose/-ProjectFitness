79b4cc50fde9c3f43ccd828206f988f51544fac2
Zemfira <vildanovazemfira@gmail.com>
2025-01-15 17:19:10 +0100

    myplans - no plans

--- client/src/components/UserPlansPage/UserPlansPage.tsx
+++ client/src/components/UserPlansPage/UserPlansPage.tsx
@@ -1,15 +1,17 @@
 import axiosInstance from '@/axiosInstance';
 import { useAppSelector } from '@/store/hooks/hooks';
-import { Box, Container, SimpleGrid } from '@chakra-ui/react';
+import { Box, Button, Container, SimpleGrid } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import UserPlanCard from './UserPlanCard';
+import { useNavigate } from 'react-router-dom';
 
 export default function UserPlansPage() {
   const { user } = useAppSelector((state) => state.appSlice);
   const [userPlans, setUserPlans] = useState([]);
   const { VITE_API } = import.meta.env;
   const [isLoading, setIsLoading] = useState(true);
-  const { userplan } = useAppSelector((state) => state.appSlice);
+  const navigate = useNavigate();
+  // const { userplan } = useAppSelector((state) => state.appSlice);
 
   //id
   useEffect(() => {
@@ -32,13 +34,16 @@ export default function UserPlansPage() {
     if (user?.id) allPlans();
   }, [user]);
 
+  const redirectPlanHandlet = () => {
+    navigate('/plans');
+  };
   // onClick={() => navigate(`/plans/${plan.planId}/days`)}
 
-  return (
-    <Container maxW="full" px={4}>
-      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={6} p={4}>
+  return userPlans.length > 0 ? (
+    <Container maxW='full' px={4}>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} p={4}>
         {userPlans.map((plan) => (
-          <Box key={plan.id} p={4} borderRadius="md">
+          <Box key={plan.id} p={4} borderRadius='md'>
             <UserPlanCard
               image={plan.Plan?.image}
               id={plan.planId}
@@ -46,6 +51,25 @@ export default function UserPlansPage() {
             />
           </Box>
         ))}
+      </SimpleGrid>
+    </Container>
+  ) : (
+    <Container maxW='full' px={4} flex={1} py={5} justifyContent='center'>
+      <SimpleGrid mx='auto'>
+        <Box justifyContent='center' py={4}>
+          Пока нет добавленных планов
+        </Box>
+        <Button
+          minW='10ch'
+          maxW='150px'
+          variant='surface'
+          colorPalette='green'
+          borderRadius='sm'
+          onClick={redirectPlanHandlet}
+          className='mt-3'
+        >
+          Добавить план
+        </Button>
       </SimpleGrid>
     </Container>
   );
