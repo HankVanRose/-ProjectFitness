24d3b4af67eaf49614036acfa084ef0b2b92be3d
Zemfira <vildanovazemfira@gmail.com>
2025-01-16 08:12:44 +0100

    Footer

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,13 +1,23 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './header/Header';
+import { Flex, Box } from '@chakra-ui/react';
 
 export default function Layout() {
   return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
+    <Flex
+      direction='column'
+      minH='100vh'
+    >
+      <Box p={4}>
+        <Header />
+      </Box>
+      <Box as='main' flex='1' p={4}>
+        <Outlet />
+      </Box>
+      <Box as='footer' p={4}>
+        <Footer />
+      </Box>
+    </Flex>
   );
 }
